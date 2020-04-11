4bd395f0ff5e38020019799116d015c866ffc009
nack43 <gurin140@gmail.com>
2020-04-11 14:53:20 +0900

    Add fonts

--- src/components/App.js
+++ src/components/App.js
@@ -1,39 +1,45 @@
-import React from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import styled, { ThemeProvider, createGlobalStyle } from "styled-components";
 
-import Signup from '../views/Signup';
-import Login from '../views/Login';
-import List from '../views/List';
-import Result from '../views/Result';
-import Survey from '../views/Survey';
-import Create from '../views/Create';
-import TitleAndDescription from '../views/TitleAndDescription';
-import Questions from '../views/Questions';
-import AddQuestion from '../views/AddQuestion';
+import Signup from "../views/Signup";
+import Login from "../views/Login";
+import List from "../views/List";
+import Result from "../views/Result";
+import Survey from "../views/Survey";
+import Create from "../views/Create";
+import TitleAndDescription from "../views/TitleAndDescription";
+import Questions from "../views/Questions";
+import AddQuestion from "../views/AddQuestion";
 
-import '../styles/App.css';
+const GlobalStyle = createGlobalStyle`
+@import url('https://fonts.googleapis.com/css2?family=Work+Sans:wght@600;700;800;900&display=swap');
+
+  body {
+    font-family: 'Work Sans', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol';
+  }
+`;
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route path="/createSurvey" component={Create} />
-        <Route path="/titleAndDescription" component={TitleAndDescription} />
-        <Route path="/questions" component={Questions} />
-        <Route path="/addQuestion" component={AddQuestion} />
-        <Route path="/signup" component={Signup} />
-        <Route path="/login" component={Login} />
-        <Route path="/list/" component={List} />
-        <Route path="/results/:id" component={Result} />
-        <Route path="/survey/:id/:page" component={Survey} />
-        <Route path="/survey/:id" component={Survey} />
-        <Route path="/" component={Login} />
-      </Switch>
-    </Router>
+    <>
+      <GlobalStyle />
+      <Router>
+        <Switch>
+          <Route path="/createSurvey" component={Create} />
+          <Route path="/titleAndDescription" component={TitleAndDescription} />
+          <Route path="/questions" component={Questions} />
+          <Route path="/addQuestion" component={AddQuestion} />
+          <Route path="/signup" component={Signup} />
+          <Route path="/login" component={Login} />
+          <Route path="/list/" component={List} />
+          <Route path="/results/:id" component={Result} />
+          <Route path="/survey/:id/:page" component={Survey} />
+          <Route path="/survey/:id" component={Survey} />
+          <Route path="/" component={Login} />
+        </Switch>
+      </Router>
+    </>
   );
 }
 
