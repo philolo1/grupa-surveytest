beb7e5b912de4fd6bc54b5a311e47709aff22b1a
nack43 <gurin140@gmail.com>
2020-04-11 13:57:29 +0900

    Add routing from questions to list

--- src/views/Questions.js
+++ src/views/Questions.js
@@ -5,5 +5,7 @@ export default () => (
   <div>
     <div>Questions</div>
       <Link to='/addQuestion'>add question</Link>
+      <br />
+      <Link to='/list'>save survey and go to list</Link>
   </div>
 )
