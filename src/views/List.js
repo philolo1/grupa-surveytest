af476327ed36069d3638f8e76139b9ae46bc1042
Huges ALEXANDRE <hugues@111studio.jp>
2020-04-13 17:11:18 +0900

    removed list selector

--- src/views/List.js
+++ src/views/List.js
@@ -1,7 +1,6 @@
 import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { observer, inject } from 'mobx-react';
-import styled from 'styled-components';
 
 import AddButton from '../components/button/AddButton';
 import {
@@ -12,21 +11,6 @@ import {
 } from '../components/signup/styles';
 import Page from '../components/Page';
 import Thumb from '../components/survey/Thumb';
-
-const StatusButton = styled.div`
-  display: flex;
-  flex: 1;
-  background: rgb(247, 247, 247);
-  border-radius: 0px;
-  border: 1px solid rgb(231, 231, 231);
-  height: 40px;
-  color: rgb(64, 64, 64);
-  font-family: SFProDisplay-Bold;
-  font-size: 16px;
-  font-weight: bold;
-  justify-content: center;
-  align-items: center;
-`;
 
 const CreateButton = () => (
   <Link to="/createSurvey" style={{ textDecoration: 'none' }}>
