71545e3d2ea90dd2a315385c9826625e1f732eb2
Patrick Klitzke <patrick.klitzke@gigsternetwork.com>
2020-04-13 13:05:28 +0900

    fix navigation after creating a survey

--- src/mobx/Survey.js
+++ src/mobx/Survey.js
@@ -1,11 +1,11 @@
-import { extendObservable } from "mobx";
-import { firestore } from "firebase";
+import { extendObservable } from 'mobx';
+import { firestore } from 'firebase';
 
-const LOADING_NUMBER = 10
+const LOADING_NUMBER = 10;
 
 export default class Survey {
-  constructor (user) {
-    this.userStore = user
+  constructor(user) {
+    this.userStore = user;
     extendObservable(this, {
       currentSurvey: null,
       currentResults: null,
@@ -13,111 +13,155 @@ export default class Survey {
       loading: false,
       maxAttained: false,
       surveys: []
-    })
+    });
   }
 
-  answer (surveyId, answers) {
-    const o = {}
-    answers.forEach((a,i) => {
-      o[a.id] = a.value
-    })
-    firestore().collection('surveys').doc(surveyId).collection('answers').doc(this.userStore.uid).set(o)
-    const a = this.userStore.answeredIds
-    a.push(surveyId)
-    firestore().collection('users').doc(this.userStore.uid).update({answeredIds: a})
+  answer(surveyId, answers) {
+    const o = {};
+    answers.forEach((a, i) => {
+      o[a.id] = a.value;
+    });
+    firestore()
+      .collection('surveys')
+      .doc(surveyId)
+      .collection('answers')
+      .doc(this.userStore.uid)
+      .set(o);
+    const a = this.userStore.answeredIds;
+    a.push(surveyId);
+    firestore()
+      .collection('users')
+      .doc(this.userStore.uid)
+      .update({ answeredIds: a });
   }
 
-  cancel (surveyId) {
+  cancel(surveyId) {
     try {
-      return firestore().collection('surveys').doc(surveyId).update({ isCancelled: true })
+      return firestore()
+        .collection('surveys')
+        .doc(surveyId)
+        .update({ isCancelled: true });
     } catch (e) {
-      console.error('Error happened while updating the survey', e)
+      console.error('Error happened while updating the survey', e);
     }
   }
 
-  create (survey, questions) {
+  async create(survey, questions) {
     // survey must contains those fields: desc, expiresAt as seconds since unix, icon, title and numberOfQuestions
     try {
-      survey.isCancelled = false
+      survey.isCancelled = false;
       if (survey.expiresAt)
-        survey.expiresAt = new firestore.Timestamp(survey.expiresAt.getTime() / 1000 || 0, 0)
-      return firestore().collection('surveys').add(survey).then(docRef => {
-        questions.forEach((q,i) => {
-          firestore().collection('surveys').doc(docRef.id).collection('questions').add(Object.assign(q, { idx: i+1 }))
-        })
-      })
+        survey.expiresAt = new firestore.Timestamp(
+          survey.expiresAt.getTime() / 1000 || 0,
+          0
+        );
+      return firestore()
+        .collection('surveys')
+        .add(survey)
+        .then(docRef => {
+          questions.forEach((q, i) => {
+            firestore()
+              .collection('surveys')
+              .doc(docRef.id)
+              .collection('questions')
+              .add(Object.assign(q, { idx: i + 1 }));
+          });
+        });
     } catch (e) {
-      console.error('Error happened while saving new survey', e)
+      console.error('Error happened while saving new survey', e);
     }
   }
 
-  results (surveyId) {
-    this.currentResults = null
-    firestore().collection('surveys').doc(surveyId).collection('answers').get().then(snap => {
-      const answers = {}, totals = {}
-      snap.forEach(a => {
-        const d = a.data()
-        Object.keys(d).forEach(q => {
-          if (!totals[q]) totals[q] = 0
-          totals[q]++
+  results(surveyId) {
+    this.currentResults = null;
+    firestore()
+      .collection('surveys')
+      .doc(surveyId)
+      .collection('answers')
+      .get()
+      .then(snap => {
+        const answers = {},
+          totals = {};
+        snap.forEach(a => {
+          const d = a.data();
+          Object.keys(d).forEach(q => {
+            if (!totals[q]) totals[q] = 0;
+            totals[q]++;
 
-          if (!answers[q]) answers[q] = {}
-          if (!answers[q][d[q]]) answers[q][d[q]] = 0
-          answers[q][d[q]]++
-        })
-      })
-      this.currentResults = {answers, totals}
-    })
+            if (!answers[q]) answers[q] = {};
+            if (!answers[q][d[q]]) answers[q][d[q]] = 0;
+            answers[q][d[q]]++;
+          });
+        });
+        this.currentResults = { answers, totals };
+      });
   }
 
-  get (id) {
-    this.currentSurvey = null
-    firestore().collection('surveys').doc(id).get().then(survey => {
-      const s = survey.data()
-      if (s.expiresAt) s.expiresAt = s.expiresAt.toMillis()
-      firestore().collection('surveys').doc(id).collection('questions').get().then(questions => {
-        const q = []
-        questions.forEach(snap => q.push(Object.assign(snap.data(), {id: snap.id})))
-        s.questions = q
-        this.currentSurvey = s
-      })
-    })
+  get(id) {
+    this.currentSurvey = null;
+    firestore()
+      .collection('surveys')
+      .doc(id)
+      .get()
+      .then(survey => {
+        const s = survey.data();
+        if (s.expiresAt) s.expiresAt = s.expiresAt.toMillis();
+        firestore()
+          .collection('surveys')
+          .doc(id)
+          .collection('questions')
+          .get()
+          .then(questions => {
+            const q = [];
+            questions.forEach(snap =>
+              q.push(Object.assign(snap.data(), { id: snap.id }))
+            );
+            s.questions = q;
+            this.currentSurvey = s;
+          });
+      });
   }
 
-  loadMore () {
+  loadMore() {
     if (this.maxAttained) {
-      console.log('can\'t load more, max attained')
-      return
+      console.log("can't load more, max attained");
+      return;
     }
 
-    this.loading = true
-    firestore().collection('surveys').where('isCancelled', '==', false).where('expiresAt', '>', firestore.Timestamp.now()).limit(LOADING_NUMBER).get().then(snap => {
-      console.log('load some surveys', snap.size)
-      this.loading = false
-      snap.forEach(doc => {
-        const addon = {uid: doc.id}
-        if (doc.data().expiresAt)
-          addon.expiresAt = doc.data().expiresAt.toMillis()
-        const s = Object.assign({}, doc.data(), addon)
-        this.surveys.push(s)
-      })
-      if (snap.size < LOADING_NUMBER) {
-        this.maxAttained = true
-      }
-    })
+    this.loading = true;
+    firestore()
+      .collection('surveys')
+      .where('isCancelled', '==', false)
+      .where('expiresAt', '>', firestore.Timestamp.now())
+      .limit(LOADING_NUMBER)
+      .get()
+      .then(snap => {
+        console.log('load some surveys', snap.size);
+        this.loading = false;
+        snap.forEach(doc => {
+          const addon = { uid: doc.id };
+          if (doc.data().expiresAt)
+            addon.expiresAt = doc.data().expiresAt.toMillis();
+          const s = Object.assign({}, doc.data(), addon);
+          this.surveys.push(s);
+        });
+        if (snap.size < LOADING_NUMBER) {
+          this.maxAttained = true;
+        }
+      });
   }
 
-  load () {
-    this.surveys = []
-    this.cursor = 0
-    this.maxAttained = false
-    this.loadMore()
+  load() {
+    this.surveys = [];
+    this.cursor = 0;
+    this.maxAttained = false;
+    this.loadMore();
   }
 
-  handleChange = (snap) => {
-    const s = []
+  handleChange = snap => {
+    const s = [];
     snap.docChanges().forEach(change => {
-      s.push(change.doc().data())
-    })
-  }
+      s.push(change.doc().data());
+    });
+  };
 }
--- src/views/Create.js
+++ src/views/Create.js
@@ -9,36 +9,46 @@ import _ from 'lodash';
 
 import AddButton from '../components/button/AddButton';
 import EmojiField from '../components/field/EmojiField';
-import History from '../tools/History';
 import Page from '../components/Page';
-import { Answers, TitleLeft, Footer, Field, SmallButton, MyRow, Button, Questions } from '../components/signup/styles';
+import {
+  Answers,
+  TitleLeft,
+  Footer,
+  Field,
+  SmallButton,
+  MyRow,
+  Button,
+  Questions
+} from '../components/signup/styles';
 
 const MyField = styled(Field)`
   label {
     margin-bottom: 10px;
   }
-  input, textarea, .field {
+  input,
+  textarea,
+  .field {
     width: auto;
   }
   textarea {
     height: 100px;
     padding: 10px 15px;
   }
-`
+`;
 
 const MyRowTitle = styled(MyRow)`
   margin-top: 20px;
   margin-bottom: 24px;
-`
+`;
 
 const RowWithBottomMargin = styled(MyRow)`
   margin-bottom: 30px;
-`
+`;
 
 const Col = styled(MyRow)`
   flex-direction: column;
   align-items: stretch;
-`
+`;
 
 const Informations = ({ init, onValidate }) => {
   const formik = useFormik({
@@ -54,9 +64,9 @@ const Informations = ({ init, onValidate }) => {
       icon: Yup.string().required('Required'),
       expiresAt: Yup.date().required('Required')
     }),
-    onSubmit: (values) => {
-      onValidate(values)
-    },
+    onSubmit: values => {
+      onValidate(values);
+    }
   });
 
   const handleClick = async () => {
@@ -77,7 +87,10 @@ const Informations = ({ init, onValidate }) => {
         <TitleLeft>Create Survey</TitleLeft>
       </MyRowTitle>
       <RowWithBottomMargin>
-        <div>Please enter a title, description. Then select an icon and choose to add a time limit or not.</div>
+        <div>
+          Please enter a title, description. Then select an icon and choose to
+          add a time limit or not.
+        </div>
       </RowWithBottomMargin>
       <form onSubmit={formik.handleSubmit}>
         <Col>
@@ -109,41 +122,57 @@ const Informations = ({ init, onValidate }) => {
         <Col>
           <MyField>
             <label htmlFor="icon">Icon</label>
-            <EmojiField selected={formik.values.icon} onChange={v => formik.setFieldValue('icon', v)} />
+            <EmojiField
+              selected={formik.values.icon}
+              onChange={v => formik.setFieldValue('icon', v)}
+            />
           </MyField>
         </Col>
         <Col>
           <MyField>
             <label htmlFor="expiresAt">Expire date</label>
-            <DayPickerInput onDayChange={day => formik.setFieldValue('expiresAt', day)} />
+            <DayPickerInput
+              onDayChange={day => formik.setFieldValue('expiresAt', day)}
+            />
           </MyField>
         </Col>
         <Footer>
           <div>1/2</div>
-          <SmallButton disabled={!formik.isValid} onClick={() => handleClick()} type="submit">
+          <SmallButton
+            disabled={!formik.isValid}
+            onClick={() => handleClick()}
+            type="submit"
+          >
             Next
           </SmallButton>
         </Footer>
       </form>
-  </>
-  )
-}
+    </>
+  );
+};
 
 const Question = ({ question }) => (
-  <Col style={{ borderBottom: '1px solid rgb(231, 231, 231)', paddingBottom: '15px' }}>
+  <Col
+    style={{
+      borderBottom: '1px solid rgb(231, 231, 231)',
+      paddingBottom: '15px'
+    }}
+  >
     <Questions>{question.question}</Questions>
-    {question.answers.map((a,i) => <Answers key={i}>{a}</Answers>)}
+    {question.answers.map((a, i) => (
+      <Answers key={i}>{a}</Answers>
+    ))}
   </Col>
-)
+);
 
 const AddQuestion = inject('create')(({ create, handleBackToQuestions }) => {
-  const [answers, setAnswers] = useState([''])
-  const [question, setQuestion] = useState('')
+  const [answers, setAnswers] = useState(['']);
+  const [question, setQuestion] = useState('');
 
   const handleSaveQuestion = () => {
-    create.addQuestion(question, _.pull(answers.slice(), '', undefined))
-    handleBackToQuestions()
-  }
+    create.addQuestion(question, _.pull(answers.slice(), '', undefined));
+    handleBackToQuestions();
+  };
 
   /*
   const handleReturnButtonPushed = () => {
@@ -152,11 +181,11 @@ const AddQuestion = inject('create')(({ create, handleBackToQuestions }) => {
   */
 
   const changeAnswers = (i, v) => {
-    const a = JSON.parse(JSON.stringify(answers))
-    a[0] = answers[0] || ''
-    a[i] = v
-    setAnswers(a) 
-  }
+    const a = JSON.parse(JSON.stringify(answers));
+    a[0] = answers[0] || '';
+    a[i] = v;
+    setAnswers(a);
+  };
 
   return (
     <>
@@ -166,7 +195,11 @@ const AddQuestion = inject('create')(({ create, handleBackToQuestions }) => {
       <Col>
         <MyField>
           <label htmlFor="question">What do you what to ask?</label>
-          <textarea name="question" value={question} onChange={e => setQuestion(e.target.value)} />
+          <textarea
+            name="question"
+            value={question}
+            onChange={e => setQuestion(e.target.value)}
+          />
         </MyField>
       </Col>
       <Col>
@@ -174,7 +207,10 @@ const AddQuestion = inject('create')(({ create, handleBackToQuestions }) => {
           <label htmlFor="answers">Multiple choice</label>
           {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map(i => (
             <input
-              style={{ display: i < 2 || answers[i-1] !== undefined  ? 'block' : 'none' }}
+              style={{
+                display:
+                  i < 2 || answers[i - 1] !== undefined ? 'block' : 'none'
+              }}
               onChange={e => changeAnswers(i, e.target.value)}
               name={i}
               key={i}
@@ -184,77 +220,112 @@ const AddQuestion = inject('create')(({ create, handleBackToQuestions }) => {
         </MyField>
       </Col>
       <Footer style={{ justifyContent: 'center' }}>
-        <Button disabled={_.pull(answers.slice(), '', undefined).length < 2 || question === ''} onClick={() => handleSaveQuestion()} type="submit">Save question</Button>
+        <Button
+          disabled={
+            _.pull(answers.slice(), '', undefined).length < 2 || question === ''
+          }
+          onClick={() => handleSaveQuestion()}
+          type="submit"
+        >
+          Save question
+        </Button>
       </Footer>
     </>
-  )
-})
+  );
+});
 
-const QuestionsView = inject('create')(observer(({ create, handleBackToSurvey, onValidate }) => {
-  const [adding, setAdding] = useState(false)
-  const q = create.questions
+const QuestionsView = inject('create')(
+  observer(({ create, handleBackToSurvey, onValidate }) => {
+    const [adding, setAdding] = useState(false);
+    const q = create.questions;
 
-  return (
-    <>
-      {adding ? <AddQuestion handleBackToQuestions={() => setAdding(false)} /> : (
-        <>
-          <MyRowTitle style={{ justifyContent: 'space-between' }}>
-            <TitleLeft>Questions</TitleLeft>
-            <AddButton onClick={() => setAdding(true)} />
-          </MyRowTitle>
-          {q.length > 0 ? q.map((qq, i) => <Question key={i} question={qq} />) : (
-            <Col style={{ alignItems: 'center', fontSize: '20px', justifyContent: 'center', flexGrow: 1 }}>
-              <div>Start by creating questions</div>
-              <div style={{ color: 'rgb(34, 179, 148)' }}>Add first question</div>
-            </Col>
-          )}
-          <Footer>
-            <div>2/2</div>
-            <SmallButton style={{ width: '142px' }} disabled={q.length < 1} onClick={() => onValidate()} type="submit">
-              Save Survery
-            </SmallButton>
-          </Footer>
-        </>
-      )}
-    </>
-  )
-}))
+    return (
+      <>
+        {adding ? (
+          <AddQuestion handleBackToQuestions={() => setAdding(false)} />
+        ) : (
+          <>
+            <MyRowTitle style={{ justifyContent: 'space-between' }}>
+              <TitleLeft>Questions</TitleLeft>
+              <AddButton onClick={() => setAdding(true)} />
+            </MyRowTitle>
+            {q.length > 0 ? (
+              q.map((qq, i) => <Question key={i} question={qq} />)
+            ) : (
+              <Col
+                style={{
+                  alignItems: 'center',
+                  fontSize: '20px',
+                  justifyContent: 'center',
+                  flexGrow: 1
+                }}
+              >
+                <div>Start by creating questions</div>
+                <div style={{ color: 'rgb(34, 179, 148)' }}>
+                  Add first question
+                </div>
+              </Col>
+            )}
+            <Footer>
+              <div>2/2</div>
+              <SmallButton
+                style={{ width: '142px' }}
+                disabled={q.length < 1}
+                onClick={() => onValidate()}
+                type="submit"
+              >
+                Save Survey
+              </SmallButton>
+            </Footer>
+          </>
+        )}
+      </>
+    );
+  })
+);
 
 class Create extends React.Component {
-  constructor (props) {
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
       loading: false,
       page: 1
-    }
-    props.create.createNew()
+    };
+    props.create.createNew();
   }
 
   handleInformationSave = v => {
-    console.log('handleInformationSave')
-    this.props.create.setInfos(v.title, v.desc, v.icon, v.expiresAt)
-    this.setState({ page: 2 })
-  }
+    console.log('handleInformationSave');
+    this.props.create.setInfos(v.title, v.desc, v.icon, v.expiresAt);
+    this.setState({ page: 2 });
+  };
 
   handleQuestionSave = () => {
-    console.log('handleQuestionsSave')
-    const { create, survey } = this.props
-    survey.create(create.survey, create.question).then(
-      History.push('/list')
-    )
-  }
+    console.log('handleQuestionsSave');
+    const { create, survey } = this.props;
+    survey.create(create.survey, create.question).then(() => {
+      this.props.history.push('/list');
+    });
+  };
 
   render() {
     return (
       <Page>
         {this.state.page === 1 ? (
-          <Informations init={this.props.create.survey} onValidate={this.handleInformationSave} />
+          <Informations
+            init={this.props.create.survey}
+            onValidate={this.handleInformationSave}
+          />
         ) : (
-          <QuestionsView loading={this.state.loading} handleBackToSurvey={() => this.setState({ page: 1 })} onValidate={this.handleQuestionSave} />
+          <QuestionsView
+            loading={this.state.loading}
+            handleBackToSurvey={() => this.setState({ page: 1 })}
+            onValidate={this.handleQuestionSave}
+          />
         )}
       </Page>
-    )
+    );
   }
 }
 
-export default inject('create', 'survey')(observer(Create))
+export default inject('create', 'survey')(observer(Create));
